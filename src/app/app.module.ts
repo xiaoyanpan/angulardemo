019e2536cef321e089efce54838412e8b65b118e
ps <ps@123>
2017-07-17 00:16:35 +0800

    项目搭建

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ProductComponent } from './product/product.component';
+import { StarsComponent } from './stars/stars.component';
+
+@NgModule({
+  declarations: [
+  // 只能声明组件,指令,管道
+    AppComponent,
+  NavbarComponent,
+  FooterComponent,
+  SearchComponent,
+  CarouselComponent,
+  ProductComponent,
+  StarsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
