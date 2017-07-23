f0dccc08cc5ff3f1a1fb2e969471c29b5cf21ace
ps <ps@123>
2017-07-23 19:05:58 +0800

    增加路由

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-stars',
-  templateUrl: './stars.component.html',
-  styleUrls: ['./stars.component.css']
+	selector: 'app-stars',
+	templateUrl: './stars.component.html',
+	styleUrls: ['./stars.component.css']
 })
 export class StarsComponent implements OnInit {
 
-  constructor() { }
+	@Input()
+	private rating: number = 0;
+	private stars: boolean[];
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+		this.stars = [];
+		for (let i = 1; i <= 5; i++) {
+			this.stars.push(i > this.rating);
+		}
+	}
 
 }
