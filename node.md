da1b68bc2f1c9ea0f2c1e033fa6c0caa434d888e
ps <ps@123>
2017-10-29 17:20:40 +0800

    配置商品详情路由

---
+++ node.md
@@ -0,0 +1,11 @@
+3 angular 路由的使用
+3.1 angular的五个对象：
+    3.1.1 Routes 路由配置
+    	  path：/user
+    	  component：A
+    3.1.2 RouterOutlet 占位符指令
+    3.1.3 Router 控制器中应用
+    3.1.4 RouterLink html模板中应用
+    3.1.5 ActivatedRoute 记录路由中的一些参数的对象
+
+3.2 重定向路由
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  private imgUrl = "http://placehold.it/320x150";
 
 	private products: Array<Product>;
 
